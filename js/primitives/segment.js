3f220808879cf20006f759a47a8a1086ef1734f6
Aditya Shibu <as2397@hw.ac.uk>
2024-06-22 14:36:13 +0400

    Completed graph

--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -6,10 +6,10 @@ class Segment {
 
     draw(ctx, width = 2, color = "black") {
         ctx.beginPath();
-        ctx.linewidth = width;
+        ctx.lineWidth = width;
         ctx.strokeStyle = color;
         ctx.moveTo(this.p1.x, this.p1.y);
-        ctx.moveTo(this.p2.x, this.p2.y);
-
+        ctx.lineTo(this.p2.x, this.p2.y);
+        ctx.stroke();
     }
 }
