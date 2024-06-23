3dc24dcdc9b453d577153cae4f4d8f5a3b3349b1
Aditya Shibu <as2397@hw.ac.uk>
2024-06-23 18:17:17 +0400

    Added function for adding a point

--- js/math/graph.js
+++ js/math/graph.js
@@ -5,6 +5,14 @@ class Graph {
         this.segments = segments;
     }
 
+    /* FUNCTION
+    Function to add a point to the graph
+    */
+    addPoint(point) {
+        // Push the point into the points array
+        this.points.push(point);
+    }
+
     draw(ctx) {
         // Loop through the segments array and draw it out
         for (const seg of this.segments) {
