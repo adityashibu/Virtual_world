bba71c176af3a9ee99d7c01bf5364e0bb1ec24db
Aditya Shibu <as2397@hw.ac.uk>
2024-06-28 00:13:09 +0400

    Refactor code to generate envelopes for all segments and draw them on canvas

--- js/world.js
+++ js/world.js
@@ -13,20 +13,35 @@ class World {
     Method to generate the envelopes for the world
     */
     generate() {
+        // Clear the envelopes
         this.envelopes.length = 0;
+        // Iterate over the segments to create the envelopes
         for (const seg of this.graph.segments) {
+            // Create a new envelope for the segment
             this.envelopes.push(
+                // Create a new envelope for the segment
                 new Envelope(seg, this.roadWidth, this.roadRoundness)
             );
         }
+
+        // Create the intersections
+        this.intersections = Polygon.break(
+            this.envelopes[0].poly,
+            this.envelopes[1].poly
+        );
     }
 
     /* METHOD
     Method to draw out the world on the canvas
     */
     draw() {
+        // Iterate over the envelopes to draw them
         for (const env of this.envelopes) {
             env.draw(ctx);
+        }
+        // Iterate over the intersections to draw them
+        for (const int of this.intersections) {
+            int.draw(ctx);
         }
     }
 }
